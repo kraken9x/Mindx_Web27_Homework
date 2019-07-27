29d34d14890c1f3916fc37393f340e92640cd708
cuongtm3101 <tranminhcuong3101@gmail.com>
2019-07-27 10:35:41 +0700

    commit homework session 3

---
+++ L3_Homework/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './public/home.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './public/about.html'));
+})
+
+app.use(express.static('public'))
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log(err);
+    }else{
+        console.log('server listen on port 3000 ...');
+    }
+});
+
